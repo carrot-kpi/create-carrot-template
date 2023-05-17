f8bb0a46ae83c7eb81e41cecbaa2195f25a844a4
Federico Luzzi <federico.luzzi@proton.me>
2023-05-17 22:45:26 +0100

    feat(carrot-scripts): swap ethers with viem and ganache with anvil (#3)
    Co-authored-by: Federico Luzzi <fedeluzzi00@gmail.com>

--- packages/carrot-scripts/lib/carrot-template.d.ts
+++ packages/carrot-scripts/lib/carrot-template.d.ts
@@ -1,5 +1,3 @@
-import { BigNumber } from "ethers";
-
 declare global {
     const __DEV__: boolean;
     const CCT_RPC_URL: string;
@@ -7,8 +5,10 @@ declare global {
     const CCT_IPFS_HTTP_API_URL: string;
     const CCT_IPFS_RPC_API_URL: string;
     const CCT_CHAIN_ID: number;
-    const CCT_TEMPLATE_ID: BigNumber;
+    const CCT_TEMPLATE_ID: number;
     const CCT_TEMPLATE_ADDRESS: string;
     const CCT_DEPLOYMENT_ACCOUNT_PRIVATE_KEY: string;
     const CCT_DEPLOYMENT_ACCOUNT_ADDRESS: string;
 }
+
+export {};
